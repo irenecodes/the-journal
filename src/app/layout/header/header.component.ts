070a08931123ea8751bfd3622c245c10405e1a48
Irene <irenesis123@gmail.com>
2021-05-16 02:10:51 -0400

    WIP

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  links = [
+    {
+      title: 'About',
+      source: '#about',
+    },
+    {
+      title: 'Blog',
+      source: 'blog',
+    },
+    {
+      title: 'Contact',
+      source: 'contact',
+    },
+  ];
 
-  constructor() { }
+  title: string = 'The Journal';
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
